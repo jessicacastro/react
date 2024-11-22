f153f311b8a4d6a28136e3e81834cae99d18d456
Jessica Castro <jessica.castro@ifood.com.br>
2024-11-21 21:55:52 -0300

    test(redux-zustand): add tests for player slice from redux store

---
+++ react-redux-zustand/src/store/slices/player.test.ts
@@ -0,0 +1,79 @@
+import { describe, expect, it } from "vitest";
+import { nextVideo, play, player as playerReducer } from "./player";
+
+const exampleState = {
+  course: {
+    modules: [
+      {
+        id: "1",
+        title: "Iniciando com React",
+        lessons: [
+          { id: "Jai8w6K_GnY", title: "CSS Modules", duration: "13:45" },
+          {
+            id: "w-DW4DhDfcw",
+            title: "Estilização do Post",
+            duration: "10:05",
+          },
+        ],
+      },
+      {
+        id: "2",
+        title: "Estrutura da aplicação",
+        lessons: [
+          {
+            id: "gE48FQXRZ_o",
+            title: "Componente: Comment",
+            duration: "13:45",
+          },
+          { id: "Ng_Vk4tBl0g", title: "Responsividade", duration: "10:05" },
+        ],
+      },
+    ],
+  },
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+};
+
+describe("Player", () => {
+  describe("when the play action is dispatched", () => {
+    it("should be able to play", () => {
+      const state = playerReducer(exampleState, play([1, 2]));
+
+      expect(state.currentModuleIndex).toBe(1);
+      expect(state.currentLessonIndex).toBe(2);
+    });
+  });
+
+  describe("when the nextVideo action is dispatched", () => {
+    it("should be able to play next lesson automatically", () => {
+      const state = playerReducer(exampleState, nextVideo());
+
+      expect(state.currentModuleIndex).toBe(0);
+      expect(state.currentLessonIndex).toBe(1);
+    });
+
+    it("should be able to jump for the next module automatically", () => {
+      const state = playerReducer(
+        { ...exampleState, currentLessonIndex: 1 },
+        nextVideo()
+      );
+
+      expect(state.currentModuleIndex).toBe(1);
+      expect(state.currentLessonIndex).toBe(0);
+    });
+  });
+
+  describe("when the nextVideo action is dispatched", () => {
+    describe("and the current module is the last one", () => {
+      it("should not update the module or the lesson", () => {
+        const state = playerReducer(
+          { ...exampleState, currentModuleIndex: 1, currentLessonIndex: 1 },
+          nextVideo()
+        );
+
+        expect(state.currentModuleIndex).toBe(1);
+        expect(state.currentLessonIndex).toBe(1);
+      });
+    });
+  });
+});
