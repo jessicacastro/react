65f0d9392eacdc5cbbe1628bd16c3ad59e9ab542
Jessica Castro <jessica.castro@ifood.com.br>
2025-03-24 18:53:04 -0300

    chore(redux-zustand): add zustand and zustand setup

---
+++ react-redux-zustand/src/zustand-store/index.ts
@@ -0,0 +1,78 @@
+import { create } from "zustand";
+import { api } from "../lib/axios";
+
+interface Course {
+  id: number;
+  modules: Array<{
+    id: number;
+    title: string;
+    lessons: Array<{
+      id: string;
+      title: string;
+      duration: string;
+    }>;
+  }>;
+}
+
+export interface PlayerState {
+  course: Course | null;
+  currentModuleIndex: number;
+  currentLessonIndex: number;
+  isLoading: boolean;
+
+  loadCourse: () => Promise<void>;
+  play: (moduleAndLessonIndex: [number, number]) => void;
+  nextVideo: () => void;
+}
+
+export const useStore = create<PlayerState>((set, get) => {
+  return {
+    course: null,
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+    isLoading: true,
+    loadCourse: async () => {
+      set({
+        isLoading: true,
+      });
+
+      const response = await api.get("/courses/1");
+
+      set({
+        course: response.data,
+        isLoading: false,
+      });
+    },
+    play: (moduleAndLessonIndex: [number, number]) => {
+      const [moduleIndex, lessonIndex] = moduleAndLessonIndex;
+
+      set({
+        currentModuleIndex: moduleIndex,
+        currentLessonIndex: lessonIndex,
+      });
+    },
+    nextVideo: () => {
+      const { currentLessonIndex, currentModuleIndex, course } = get();
+
+      const nextLessonIndex = currentLessonIndex + 1;
+      const nextLesson =
+        course?.modules[currentModuleIndex].lessons[nextLessonIndex];
+      const nextModuleIndex = currentModuleIndex + 1;
+      const nextModule = course?.modules[nextModuleIndex];
+
+      if (nextLesson) {
+        set({
+          currentLessonIndex: nextLessonIndex,
+        });
+        return;
+      }
+
+      if (nextModule) {
+        set({
+          currentModuleIndex: nextModuleIndex,
+          currentLessonIndex: 0,
+        });
+      }
+    },
+  };
+});
