136845ad62849ca8f90cd7a6e24ca39e09410438
Jessica Castro <Jessica.castros@outlook.com>
2025-03-24 18:54:39 -0300

    refactor(redux-zustand): remove redux from app component

--- react-redux-zustand/src/App.tsx
+++ react-redux-zustand/src/App.tsx
@@ -1,13 +1,12 @@
-import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./store";
 import { Player } from "./pages/Player";
 
 import './styles/global.css';
 
 export const App = () => {
   return (
-    <ReduxProvider store={store}>
-      <Player />
-    </ReduxProvider>
+    // <ReduxProvider store={store}>
+    //   <Player />
+    // </ReduxProvider>
+    <Player />
   );
 }
